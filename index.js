970bf4846ff1f97623fe5d2b73f773903ddd3984
Tim <TG908@users.noreply.github.com>
2016-02-29 12:03:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,43 @@ PanasonicTV.prototype.getOn = function(callback) {
 
 PanasonicTV.prototype.setOn = function(on, callback) {
 
+var url = "/nrc/control_0";
+
+var action = ;
+var command = ;
+
+var body = "<?xml version='1.0' encoding='utf-8'?> \
+	   <s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/' s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'> \
+	    <s:Body> \
+	     <u:"+action+" xmlns:u='urn:"+urn+"'> \
+	      "+command+" \
+	     </u:"+action+"> \
+	    </s:Body> \
+	   </s:Envelope>";
+	   
+	 var postRequest = {
+	    host: ip,
+	    path: url,
+	    port: 55000,
+	    method: "POST",
+	    headers: {
+	      'Content-Length': body.length,
+	      'Content-Type': 'text/xml; charset="utf-8"',
+	      'SOAPACTION': '"urn:'+urn+'#'+action+'"'
+	    }
+	 };
+
+  var req = http.request(postRequest, function(res) {
+	    res.setEncoding('utf8');
+	    res.on('data', self.callback);
+	  });
+
+
   if(on){
+    
+	  req.write(body);
+	  req.end();
+	};
     
   } else {
     
