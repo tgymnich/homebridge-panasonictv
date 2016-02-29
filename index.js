28b0f3637c3f4a9ce47baed0e5eadc8229b2cb87
Tim <TG908@users.noreply.github.com>
2016-02-29 18:13:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ PanasonicTV.prototype.setOn = function(on, callback) {
 	var req = http.request(postRequest, function(res) {
 	    res.setEncoding('utf8');
 	    res.on('data', function(chunk){
-		console.log("body"+ chunk); 	
+		console.log("request sent"); 	
 	    });
 	  });
 
